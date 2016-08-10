7533651d9c35d44f6923f6aa96d05209b211625f
Baker <ybaker@cscfed.root.ad>
2016-08-10 16:26:20 -0500

    no message

--- server/src/main/java/com/mysite/selenium/MyWebsite.java
+++ server/src/main/java/com/mysite/selenium/MyWebsite.java
@@ -8,6 +8,6 @@ public class MyWebsite
 		public static final By LOC_LNK_MESSAGENAVIGATOR = By.xpath("/html/body/my-app/nav/a[2]");
 		public static final By LOC_BTN_SENDEMAILBUTTON = By.xpath("/html/body/my-app/ses-component/div/button");
 		public static final By LOC_BTN_SENDMESSAGEBUTTON = By.xpath("/html/body/my-app/sqs-component/div/button[1]");
-		public static final By LOC_BTN_RECEIVEMESSAGEBUTTON = By.xpath("/html/body/my-app/sqs-component/div/button[2]");		
+		public static final By LOC_BTN_RECEIVEMESSAGEBUTTON = By.xpath("/html/body/my-app/sqs-component/div/button[2]");	
 		
 }
